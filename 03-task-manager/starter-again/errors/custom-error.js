6c14577997390962eb7bd2d1630e0309158488a8
dpauld <imdpauld@gmail.com>
2022-03-25 02:41:23 +0600

    updated comments

--- 03-task-manager/starter-again/errors/custom-error.js
+++ 03-task-manager/starter-again/errors/custom-error.js
@@ -1,5 +1,6 @@
 //this is a custom class used to throw all types of custom errors. Using this it becomes easier to handle different types of error with just if else condition based on their status code or based on the type of error (customAPIError or other error)
 //Where it has been use? : middlewares/error-handler.js,  controllers/task.js
+
 class customAPIError extends Error {
   constructor(message, statusCode) {
     super(message);
